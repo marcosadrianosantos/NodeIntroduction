2df9786c26b045e317674a6d7ed44281bdb6b313
marcos santos <marcoscoala14@gmail.com>
2021-05-25 23:36:00 -0300

    Adicionando dependencias

---
+++ src/index.js
@@ -0,0 +1,52 @@
+const { request, response } = require('express');
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+
+/**
+ * Tipos de Parâmetros
+ * Route Params => Identificar um recurso p/ editar ou deletar ou buscar
+ * Query Params => Obter Paginação / Filtro
+ * Body Params => Os objetos que passamos para inserção / Alteração de recurso (JSON)
+ */
+
+app.get("/courses", (request, response) =>{
+    const query = request.query;
+    console.log(query);
+    return response.json([
+        "Curso 1", "Curso 2", "Curso 3"
+    ]);
+});
+
+app.post("/courses", (request, response) => {
+    const body = request.body;
+    console.log(body);
+    return response.json([
+        "Curso 1", "Curso 2", "Curso 3", "Curso 4"
+    ]);
+});
+
+app.put("/courses/:id", (request, response) => {
+    const {id} = request.params;
+    console.log(id); 
+    return response.json([
+        "Curso 1 update", "Curso 2", "Curso 3", "Curso 4"
+    ]);
+});
+
+app.patch("/courses/:id", (request, response) => {
+    return response.json([
+        "Curso 1 update", "Curso 2 update patch", "Curso 3", "Curso 4"
+    ]);
+});
+
+app.delete("/courses/:id", (request, response) => {
+    return response.json([
+        "Curso 1 update", "Curso 2 update patch", "Curso 4"
+    ])
+})
+
+app.listen(3333);
